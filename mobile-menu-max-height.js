ebe8089be69a705fa7badc4a4ca9cf5f9a0c2a2b
cheba91 <58878791+cheba91@users.noreply.github.com>
2023-12-24 14:19:36 +0100

    Create mobile-menu-max-height.js

---
+++ mobile-menu-max-height.js
@@ -0,0 +1,10 @@
+// Display as normal dropdown, but limit it's height to bottom of the screen. Offsets mobile URL bar
+  document.addEventListener('DOMContentLoaded', () => {
+    if (window.innerWidth < 992) {
+      const nav = document.querySelector('.nav');
+      const navMenu = document.querySelector('.nav__menu');
+      const setMaxHeight = () => nav && navMenu && (navMenu.style.maxHeight = `${window.outerHeight - nav.offsetHeight}px`);
+      setMaxHeight();
+      window.addEventListener('resize', setMaxHeight);
+    }
+  });
